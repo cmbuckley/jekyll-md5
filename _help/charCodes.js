5c0144a8f2453cccefee6acc80c5d3fe81849528
Chris Buckley <chris@cmbuckley.co.uk>
2021-03-04 23:33:23 +0000

    actually lets have percent-encoded in the yml

--- _help/charCodes.js
+++ _help/charCodes.js
@@ -4,7 +4,15 @@ const fs = require('fs');
 let output = '';
 
 for (let i = 0; i <= 65535; i++) {
-    output += '- "' + (i == 10  ? '\\n' : String.fromCharCode(i)) + '"\n';
+    output += '- "';
+
+    try {
+        output += encodeURI(String.fromCharCode(i));
+    } catch (e) {
+        output += "ERR";
+    }
+
+    output += '"\n';
 }
 
 fs.writeFileSync('_data/char_codes.yml', output);
