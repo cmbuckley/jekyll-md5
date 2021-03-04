69e912c5f3b076c1d9cc58e08b81d1ed054711cd
Chris Buckley <chris@cmbuckley.co.uk>
2021-03-04 23:20:09 +0000

    Update to char_codes

--- _help/charCodes.js
+++ _help/charCodes.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 let output = '';
 
@@ -5,4 +7,4 @@ for (let i = 0; i <= 65535; i++) {
     output += '- "' + (i == 10  ? '\\n' : String.fromCharCode(i)) + '"\n';
 }
 
-fs.writeFileSync('_data/charCodes.yml', output);
+fs.writeFileSync('_data/char_codes.yml', output);
