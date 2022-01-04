4c477ef22c9e42a3dd350908b6ee9054adce63c5
Chris Buckley <chris@cmbuckley.co.uk>
2022-01-04 00:59:17 +0000

    commit helper

---
+++ _help/test.js
@@ -0,0 +1,38 @@
+function binl2rstr(input) {
+    var i
+    var output = ''
+    var length32 = input.length * 32
+    for (i = 0; i < length32; i += 8) {
+      output += String.fromCharCode((input[i >> 5] >>> i % 32) & 0xff)
+    }
+    return output
+}
+
+function rstr2binl(input) {
+    var i
+    var output = []
+    output[(input.length >> 2) - 1] = undefined
+    for (i = 0; i < output.length; i += 1) {
+      output[i] = 0
+    }
+    var length8 = input.length * 8
+    for (i = 0; i < length8; i += 8) {
+      output[i >> 5] |= (input.charCodeAt(i / 8) & 0xff) << i % 32
+    }
+    return output
+}
+
+function binlMD5(x, len) {
+	console.log(x);
+    /* append padding */
+    x[len >> 5] |= 0x80 << len % 32
+    x[(((len + 64) >>> 9) << 4) + 14] = len
+	console.log(x);
+    console.log(x.length);
+}
+
+function md5(s) {
+	return binl2rstr(binlMD5(rstr2binl(s), s.length * 8))
+}
+
+console.log(md5('Hello'));
